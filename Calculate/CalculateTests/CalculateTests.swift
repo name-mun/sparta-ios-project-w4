800e7fcb6eebbd80a494938f7c0afcf4db5e729a
name-mun <tains9153@gmail.com>
2024-11-14 17:32:06 +0900

    🎉 Initial commit

---
+++ Calculate/CalculateTests/CalculateTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateTests.swift
+//  CalculateTests
+//
+//  Created by mun on 11/14/24.
+//
+
+import Testing
+@testable import Calculate
+
+struct CalculateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

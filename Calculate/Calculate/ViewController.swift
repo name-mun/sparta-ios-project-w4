98d6f3fb67c72a2297d44ea4f7d3ab31b2ede99f
name-mun <tains9153@gmail.com>
2024-11-18 15:51:16 +0900

    ✨ Lever 3 기능 구현 - 4개의 가로 스택뷰가 있는 세로 스택뷰 생성

--- Calculate/Calculate/ViewController.swift
+++ Calculate/Calculate/ViewController.swift
@@ -9,79 +9,101 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    
-    let label = UILabel()
-    let stackView = UIStackView()
-    let buttons = [UIButton(), UIButton(), UIButton(), UIButton()]
-    let buttonDatas = ["7", "8", "9", "+"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        configureUI()
-        configureLayout()
+
+  let label = UILabel()
+  var verticalStackView = UIStackView()
+  let buttonDatas = [["7", "8", "9", "+"], ["4", "5", "6", "-"], ["1", "2", "3", "*"], ["AC", "0", "=", "/"]]
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    configureUI()
+    configureLayout()
+  }
+
+  // UI 설정
+  private func configureUI() {
+    view.backgroundColor = .black
+
+    [label, verticalStackView].forEach{
+      view.addSubview($0)
     }
-    
-    // UI 설정
-    private func configureUI() {
-        view.backgroundColor = .black
-        configureLabel()
-        configureStackView()
-        
-        [label, stackView].forEach{
-            view.addSubview($0)
-        }
+
+    configureLabel()
+    configureVerticalStackView()
+  }
+
+  // label 스타일 설정
+  private func configureLabel() {
+    label.text = "12345"
+    label.textColor = .white
+    label.textAlignment = .right
+    label.font = .boldSystemFont(ofSize: 60)
+  }
+
+  // VerticalStackView 스타일 설정
+  private func configureVerticalStackView() {
+    for i in 0..<4 {
+      let view = makeHorizontalStackView(i)
+      verticalStackView.addArrangedSubview(view)
     }
-    
-    // label 스타일 설정
-    private func configureLabel() {
-        label.text = "12345"
-        label.textColor = .white
-        label.textAlignment = .right
-        label.font = .boldSystemFont(ofSize: 60)
+
+    verticalStackView.axis = .vertical
+    verticalStackView.backgroundColor = .black
+    verticalStackView.spacing = 10
+    verticalStackView.distribution = .fillEqually
+  }
+
+  // horizontalStackView 생성
+  private func makeHorizontalStackView(_ verIndex: Int) -> UIStackView {
+    let stackview = UIStackView()
+
+    stackview.axis = .horizontal
+    stackview.backgroundColor = .black
+    stackview.spacing = 10
+    stackview.distribution = .fillEqually
+
+    for i in 0..<4 {
+      stackview.addArrangedSubview(makeButton(verIndex: verIndex, horIndex: i))
     }
-    
-    private func configureStackView() {
-        stackView.axis = .horizontal
-        stackView.backgroundColor = .black
-        stackView.spacing = 10
-        stackView.distribution = .fillEqually
-        
-        configureButton()
+
+    stackview.snp.makeConstraints() {
+      $0.width.equalTo(350)
     }
-    
-    private func configureButton() {
-        for i in buttons.indices {
-            buttons[i].setTitle(buttonDatas[i], for: .normal)
-            buttons[i].titleLabel?.font = .boldSystemFont(ofSize: 30)
-            buttons[i].backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
-            stackView.addArrangedSubview(buttons[i])
-        }
+
+    return stackview
+  }
+
+  // 버튼 생성
+  private func makeButton(verIndex: Int, horIndex: Int) -> UIButton {
+    let keypad = UIButton()
+
+    keypad.setTitle(buttonDatas[verIndex][horIndex], for: .normal)
+    keypad.titleLabel?.font = .boldSystemFont(ofSize: 30)
+    keypad.backgroundColor = UIColor(red: 58/255, green: 58/255, blue: 58/255, alpha: 1.0)
+
+    keypad.snp.makeConstraints() {
+      $0.width.height.equalTo(80)
     }
-    
-    // 레이아웃 설정
-    private func configureLayout() {
-        label.snp.makeConstraints {
-            $0.leading.trailing.equalToSuperview().inset(30)
-            $0.top.equalToSuperview().inset(200)
-            $0.height.equalTo(100)
-        }
-        
-        stackView.snp.makeConstraints {
-            $0.height.equalTo(80)
-            $0.top.equalTo(label.snp.bottom).offset(50)
-            $0.centerX.equalToSuperview()
-        }
-        
-        for button in buttons {
-            button.snp.makeConstraints {
-                $0.height.width.equalTo(80)
-            }
-        }
-        
+
+    return keypad
+  }
+
+  // 레이아웃 설정
+  private func configureLayout() {
+    label.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(30)
+      $0.top.equalToSuperview().inset(200)
+      $0.height.equalTo(100)
     }
+
+    verticalStackView.snp.makeConstraints {
+      $0.top.equalTo(label.snp.bottom).offset(60)
+      $0.centerX.equalToSuperview()
+    }
+  }
 }
 
 #Preview("ViewController") {
-    ViewController()
+  ViewController()
 }
